984adea4775b76df9367e6c1229d936696eb56ce
Gustavo Henrique <gustavosilva010304@gmail.com>
2026-03-30 22:13:55 -0300

    primeiro salvamento

---
+++ src/components/List/index.jsx
@@ -0,0 +1,23 @@
+import './style.css'
+function List() {
+  return (
+    <section>
+      <h2 id="exemplo-h2" >Seção de listas</h2>
+
+
+      <h3 id="ul" >lista não ordenada</h3>
+      <ul>
+        <li>Batata</li>
+        <li>Pão</li>
+        <li>Arroz</li>
+      </ul>
+      <h3>lista ordenada</h3>
+      <ul>
+        <li>Rotiwels</li>
+        <li>Barbye</li>
+        <li>Poquemons</li>
+      </ul>
+    </section>
+  )
+}
+export default List
